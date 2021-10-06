55ca0ed4a056d4d9c9c5949929464dbf4d809f15
Dark-super-me <87744886+Dark-super-me@users.noreply.github.com>
2021-10-06 21:14:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,8 +18,30 @@ def start(client, message):
   rep = =f"Hi {message.from_user.username} \n <b> I am one simple bot  made by @Bro_isDarkal \nPls click the below buttons to get a hint about this bot !\n\n By @Animes_Encoded.<b> \n",
   message.reply_text(
     text=rep,
-    rep =f"Hi {message.from_user.username} \n <b> I am one simple bot  made by @Bro_isDarkal \nPls click the below buttons to get a hint about this bot !\n\n By @Animes_Encoded.<b> \n",
-    message.reply_text(
-        text=rep,
-        quote=False,
+    quote=False,
+    reply_markup=InlineKeyboardMarkup(button))
+
+@ffmpeg.on_callback_query()
+async def callback_handlers(_, event: CallbackQuery):
+    if "ihelp" in event.data:
+       await event.message.edit(
+           message.chat.id,
+           text="Checkout The Available Commands Here \n\n Do Follow @Animes_Encoded\n\n If You Find This Bot Usefull❤️",
+           reply_markup=InlineKeyboardMarkup(bbutton))
+    elif "iguide" in event.data:
+       await event.message.edit(
+         message.chat.id,
+         text="Right now the bot can only compress MKV formated files and the file must be Telegram Video or Telegram Document type",
+         reply_markup=InlineKeyboardMarkup(bbutton))
+    elif "beck" in event.data:
+       await event.message.edit(
+          message.chat.id,
+          text="Hi \nIam next generation video encoder bot!\n\nUpdates will come soon\n\nnMaintained by • @Animes_Encoded",
+          reply_markup=InlineKeyboardMarkup(button))
+        
+          
+       
+          
+# run the application        
+bot.run()
       
