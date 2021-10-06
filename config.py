f691104fa6c63f479a1776c149ea452d87fc8a27
Dark-super-me <87744886+Dark-super-me@users.noreply.github.com>
2021-10-06 17:52:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,9 @@ class Config:
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
+app = Client(
+    'ffmpeg',
+    bot_token = Config.BOT_TOKEN,
+    api_id = Config.API_ID,
+    api_hash = Config.API_HASH
+)
